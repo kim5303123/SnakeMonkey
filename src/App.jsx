7f0be4c79bc39b078ba60d77dc6cd8e07676ef27
kim5303123 <kim5303123@gmail.com>
2025-01-09 14:12:53 +0900

    commit
    배포 폴더명 변경 build -> dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cover from "./components/Cover";
 import Intro from "./components/Intro";
 // import Gallery from "./components/Gallery";
@@ -13,8 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Cover} />
           <Route path="/intro" component={Intro} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/location" component={Location} />
+          {/* <Route path="/gallery" component={Gallery} /> */}
+          {/* <Route path="/location" component={Location} /> */}
           <Route path="/rsvp" component={RSVP} />
         </Switch>
       </div>
